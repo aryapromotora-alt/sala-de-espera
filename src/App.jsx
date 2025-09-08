3bc43835327499f163e99e76afdc00ba26391085
aryapromotora-alt <aryapromotora@gmail.com>
2025-09-08 18:00:13 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -548,18 +548,27 @@ function App() {
         </div>
 
         {/* RSS Ticker na parte inferior - apenas se existir */}
-        {rssTickerItem && (
+{rssTickerItem && (
   <div
     className="fixed bottom-0 left-0 w-full z-50"
-    style={{ background: 'transparent' }}
+    style={{
+      background: 'transparent',
+      margin: 0,
+      padding: 0,
+      height: '55px', // ajuste para a altura real do ticker
+      boxSizing: 'border-box'
+    }}
   >
     <iframe
       src={rssTickerItem.url}
       style={{
         width: '100%',
-        height: '60px',
+        height: '55px', // igual ao container!
         border: 'none',
-        backgroundColor: 'transparent'
+        backgroundColor: 'transparent',
+        margin: 0,
+        padding: 0,
+        display: 'block'
       }}
       allowtransparency="true"
       title={rssTickerItem.title || 'RSS Ticker'}
